698156493d7fa1c0c3530954957ac1f5fc9c1ca4
Pawel Owiesek <Owiesek.p@gmail.com>
2023-10-09 12:13:54 +0200

    fetch data from weather api

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ const wind = document.querySelector(".wind");
 const API_UNITS = `&units=metric`;
 const API_KEY = `&appid=0e5a65e429c3e0752390f805968a4188`;
 const API_LINK = `https://api.openweathermap.org/data/2.5/weather?q=`;
+
+const getApiData = () => {
+  const city = input.value || "Vlissingen";
+  const API = API_LINK + city + API_KEY + API_UNITS;
+  axios.get(API).then((response) => {
+    const data = response.data;
+    cityName.textContent = data.name;
+    const temp = data.main.temp;
+    temperature.textContent = temp.toFixed(0) + "°C";
+    const clouds = data.weather[0].main;
+    weather.textContent = clouds;
+    const humid = data.main.humidity + "%";
+    humidity.textContent = humid;
+    const winds = data.wind.speed.toFixed(0) + "km/h";
+    wind.textContent = winds;
+  });
+};
+getApiData();
